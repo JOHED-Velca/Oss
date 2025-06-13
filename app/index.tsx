e8e1527275309673725fb5027e08865a96a2f74b
Jonathan Velasquez <johedvelca@gmail.com>
2025-06-12 22:53:29 -0400

    Bottom NavBar v1  updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-blue-600 font-bold text-5xl">Welcome</Text>
+      <Text className="text-blue-600 font-bold text-5xl">Welcome!</Text>
+      <StatusBar style="auto"/>
+      <Link href="/home" style={{ color: 'red'}}>Go to Profile</Link>
     </View>
   );
 }
